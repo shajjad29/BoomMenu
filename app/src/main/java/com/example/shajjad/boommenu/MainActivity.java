6f6b0c8421f6783df1485d8a30097e3b62839427
shajjad29 <shajjadhowlader@gmail.com>
2017-10-29 02:52:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/shajjad/boommenu/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.shajjad.boommenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nightonke.boommenu.BoomButtons.ButtonPlaceEnum;
+import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.TextOutsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.ButtonEnum;
+import com.nightonke.boommenu.Piece.PiecePlaceEnum;
+
+public class MainActivity extends AppCompatActivity {
+    BoomMenuButton bmb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bmb = findViewById(R.id.boomMenu);
+        
+        bmb.setButtonEnum(ButtonEnum.Ham);
+        bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_3_1);
+        bmb.setButtonPlaceEnum(ButtonPlaceEnum.HAM_3);
+
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            HamButton.Builder builder = new HamButton.Builder()
+                    .normalImageRes(R.drawable.gender)
+                    .normalText("Search "+(i+1))
+                    .subNormalText("Details");
+            bmb.addBuilder(builder);
+        }
+    }
+}

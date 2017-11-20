395fcb7f3e6fd79562ca1039fa1e592268f03891
shajjad29 <shajjadhowlader@gmail.com>
2017-11-21 00:54:35 +0600

    chnge

--- app/src/main/java/com/example/shajjad/boommenu/MainActivity.java
+++ app/src/main/java/com/example/shajjad/boommenu/MainActivity.java
@@ -23,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
         bmb.setPiecePlaceEnum(PiecePlaceEnum.DOT_3_1);
         bmb.setButtonPlaceEnum(ButtonPlaceEnum.HAM_3);
 
+
         for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
             HamButton.Builder builder = new HamButton.Builder()
                     .normalImageRes(R.drawable.gender)
                     .normalText("Search "+(i+1))
                     .subNormalText("Details");
             bmb.addBuilder(builder);
+            //shajjjad howlader
         }
+
     }
 }
